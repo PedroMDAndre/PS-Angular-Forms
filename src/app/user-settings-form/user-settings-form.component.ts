e7e23652b7a1f2344697016647d418870c6dbf69
PedroMDAndre <pedro.mda@gmail.com>
2021-04-30 16:09:00 +0100

    error handling

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -18,8 +19,10 @@ export class UserSettingsFormComponent implements OnInit {
   };
 
   userSettings: UserSettings = { ...this.originalUserSettings };
+  postError: boolean = false;
+  postErrorMessage: string = "";
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
   }
@@ -27,10 +30,27 @@ export class UserSettingsFormComponent implements OnInit {
 
   onSubmit(form: NgForm) {
     console.log("in onSubmit: ", form.valid);
+
+    if (form.valid) {
+      this.dataService.postUserSettingsForm(this.userSettings)
+        .subscribe(
+          result => console.log("success", result),
+          error => this.onHttpError(error)
+        );
+    } else { 
+      this.postError = true;
+      this.postErrorMessage = "Please fix the above errors.";
+    }
   }
 
-  onBlur(field: NgModel){
+  onBlur(field: NgModel) {
     console.log("in onBlur ", field.valid);
 
+  }
+
+  onHttpError(errorResponse: any) {
+    console.log("error", errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
   }
 }
